454d5efebb449ccf4ec8aa6f895b44bab026ae0b
wojtek <wojtekpietraszuk@gmail.com>
2022-04-29 15:43:56 +0200

    Add file with all methods

--- scripts/clustering_cars.R
+++ scripts/clustering_cars.R
@@ -1,18 +1,17 @@
 install.packages("e1071")
-install.packages("factiextra")
+install.packages("factoextra")
 install.packages("cluster")
 library(factoextra)
 library(cluster)
 library(e1071) 
+library(RCurl)
+library(kohonen)
 
+# pobieranie danych
+cars_gh <- getURL("https://raw.githubusercontent.com/JerzyMarczewski/MAD-Projekt-1/main/cars_multi.csv")
+dt <- read.csv(text = cars_gh, header = T, sep = ',', dec = '.')
 
-# UWAGA W PLIKU cars_mutli.csv ręcznie zamieniłem znak '?' na ''
-
-dt <- read.csv(
-  "C:\\Users\\Jerzy\\Documents\\nauka\\MAD\\cars_multi.csv", 
-  header = T, 
-  sep = ',',
-  dec = '.')
+set.seed(100)
 
 # usuwamy puste wiersze 
 dt <- na.omit(dt)
@@ -36,13 +35,70 @@ sapply(dt, skewness)
 # standaryzujemy dane
 dt <- scale(dt)
 
+
+################################################################################
 # metoda łokciowa
 fviz_nbclust(dt, kmeans, method = "wss")
 
 # metoda silhouette
+windows()
 fviz_nbclust(dt, kmeans, method = "silhouette")
 
 # k-srednich
 options(ggrepel.max.overlaps = 0)
-km <- kmeans(dt, centers = 3)
+km <- kmeans(dt, centers = 2)
 fviz_cluster(km, data = dt, repel = T)
+
+
+################################################################################
+# metoda warda
+odleglosci <- dist(dt)
+grupy <- agnes(odleglosci, method = "ward")
+windows()
+fviz_dend(grupy, k = 4, rect = TRUE, main = "Metoda Ward")
+
+# metoda complete
+grupy_complete <- agnes(odleglosci, method = "complete")
+fviz_dend(grupy_complete, k = 4, rect = TRUE, main = "Metoda complete")
+
+# metoda single
+grupy_single <- agnes(odleglosci, method = "single")
+fviz_dend(grupy_single, k = 4, rect = TRUE, main = "Metoda single")
+
+# metoda average
+grupy_average <- agnes(odleglosci, method = "average")
+fviz_dend(grupy_average, k = 4, rect = TRUE, main = "Metoda average")
+
+
+################################################################################
+# metoda Self-organising maps
+cars_SOM <- as.matrix(dt)
+cars_grid <- somgrid(xdim = 6, ydim = 6, topo = "hexagonal")
+set.seed(100)
+cars_SOM_model <- som(cars_SOM, grid = cars_grid)
+
+
+# Plot our results
+# Plot type 1: counts
+plot(cars_SOM_model, type = "counts")
+# bardziej czerwone maja mniej obserwacji, żółty wiecej obserwacji, szary brak obserwacji
+# zmniejszyc liczbe gridów, bo wystepuja szare okregi
+
+
+# Plot type 2: heatmap
+# Dla sepal.width
+plot(cars_SOM_model, type = "property",
+     property = getCodes(cars_SOM_model)[,4],
+     main = colnames(dt)[4])
+
+
+# Plot type 3: fan diagram
+plot(cars_SOM_model, type = "codes")
+
+
+# Plot type 4: trainig progress
+plot(cars_SOM_model, type = "changes")
+
+
+# Plot type 5: neighbour distance
+plot(cars_SOM_model, type = "dist.neighbours")
